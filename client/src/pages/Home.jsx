c1a093a6449d69d2fd9a57b99561a9e9d53cdc90
Manish <manish2003sharma@gmail.com>
2025-08-15 16:02:36 +0530

    pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import '../App.css'
 const Home = () => {
     return (
         <div>
-
+            <h1>Home</h1>
         </div>
     );
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import './styles/Register.css'
+import './styles/Register.css';
+import { FaFacebookF, FaGoogle, FaGithub } from "react-icons/fa";
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Register = () => {
-    return (
-        <div>
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState("");
+    const [dob, setDOB] = useState('');
+    const [weight, setWeight] = useState('');
+    const [height, setHeight] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [gender, setGender] = useState('');
+    const [profilePic, setProfilePic] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [heightUnit, setHwightUnit] = useState('');
+    const [weightUnit, setWeightUnit] = useState('');
 
+    return (
+        <div className="signup-container">
+            <div className="signup-left">
+                <h1 className='signup-heading'>
+                    Zyvo
+                </h1>
+            </div>
         </div>
     );
 }
