3123ce7425a1a9502f3c8c97cf84a8a2c72c25f7
Manish <manish2003sharma@gmail.com>
2025-08-24 22:51:10 +0530

    routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const updateProfileController = require('../controllers/updateProfileController');
 
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
-
+router.post('/updatePass', authController.updatePassword);
+router.post('/updateProfile', updateProfileController.updateProfile);
+router.post('/profilePic', updateProfileController.updateProfilePic);
 module.exports = router;
