9e6885afec9d8879cac808187097cb0750f2fa29
Manish <manish2003sharma@gmail.com>
2025-08-24 22:50:49 +0530

    db

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI,
+    // {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    // }
+)
     .then(() => console.log('MongoDB Connected'))
     .catch((err) => console.error('MongoDB Connection Error:', err));
 
